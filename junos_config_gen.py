0d1e94ac549741b4e75c214147efac867f179d07
R4v3nH0lm <zgothmog@gmail.com>
2016-10-11 00:58:39 -0700

    added to script file

--- junos_config_gen.py
+++ junos_config_gen.py
@@ -1,2 +1,18 @@
-#Import librarys
+# Import librarys
+#################
 import jinja2
+
+
+# Global Variables
+##################
+
+# Script Actions
+################
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
